a3fad2dc54cd6866df72155e4256240cb111b868
Neepa <pandyaneepa23@gmail.com>
2025-02-27 23:51:44 +0000

    changed for github workflow

--- sauce_lib.py
+++ sauce_lib.py
@@ -26,6 +26,11 @@ class TestSauce:
             driver = webdriver.Edge()
         elif browser == "chrome":
             options = Options()
+            options.add_argument("--headless")  
+            options.add_argument("--no-sandbox")  
+            options.add_argument("--disable-dev-shm-usage")
+            options.add_argument("--disable-gpu")  
+            options.add_argument("--window-size=1920,1080") 
             options.add_argument("start-maximized")
             driver = webdriver.Chrome(
                 service=Service(ChromeDriverManager().install()), options=options
